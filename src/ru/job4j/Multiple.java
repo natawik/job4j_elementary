27645d86792f58f2b94c5c0b52391fef067b602d
natawik <n.klabukova.spb@gmail.com>
2020-04-04 22:26:53 +0300

    3.3. Разбор первой программы.[#253312]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        int[] mult = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i : mult) {
+            System.out.println(i);
+        }
+    }
+}
